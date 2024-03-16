3f52e9c004350aab14bb61cc64cd9acfac51e39b
Karged <andrii.briezghunov@gmail.com>
2024-03-16 18:48:30 +0200

    calculator done

--- Home_works.js
+++ Home_works.js
@@ -1,2 +1,25 @@
-let text = 'text';
-console.log(text);
+let age = 21;
+let age1 = age % 10;
+let message = '';
+if (age < 1 || age > 99) {
+    message = " років? та ну не може такого бути!";
+}
+else if (age == 1) {
+    message = " рік";
+}
+else if (age < 5 && age > 1) {
+    message = " роки";
+}
+else if (age > 4 && age < 21) {
+    message = " років";
+}
+else if (age > 20 && age1 == 1) {
+    message = " рік";
+}
+else if (age > 20 && age1 > 1 && age1 < 5) {
+    message = " роки";
+}
+else {
+    message = " років";
+};
+console.log("тобі " + age + message);
