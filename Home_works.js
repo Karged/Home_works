6fdff7cc611e3620658b72d3b23e91a1d7a5746d
Karged <andrii.briezghunov@gmail.com>
2024-03-18 14:39:13 +0200

    fixes 3

--- Home_works.js
+++ Home_works.js
@@ -15,3 +15,12 @@ for (let i = 1; i <= m; i++) {
     console.log(kym.repeat((m - i)));
 }
 console.log("__________________________");
+
+let o = 5;
+console.log("Random figure");
+console.log("__________________________");
+for (let i = 1; i <= o; i++) {
+    let kym = "$ ";
+    console.log(kym.repeat((o + 3 + i)));
+}
+console.log("__________________________");
