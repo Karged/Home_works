a6162ce690dbf5df0e04869d2874c4215ea86efd
Karged <andrii.briezghunov@gmail.com>
2024-03-18 22:50:52 +0200

    new files with homeworks 2

--- Home_works.js
+++ Home_works.js
@@ -26,26 +26,3 @@ for (let i = 1; i <= o; i++) {
     console.log(kym.repeat((o + 3 + i)));
 }
 console.log("__________________________");
-
-// КРАТНІСТЬ ЧИСЕЛ
-
-let sum = 0;
-for (var i = 1; i <= 100; i++) {
-    if (i % 3 != 0)
-        sum = sum + i;
-    else
-        sum = sum;
-  }
-console.log("Пораховано за допомогою for. Сума = " + sum);
-
-
-
-let sim = 0;
-let n = 0;
-while (n <= 100) {
-        if (n % 3 != 0)
-        sim = sim + n;
-        n++;
-    continue;
-}
-console.log("Пораховано за допомогою while. Сума = " + sim);
--- Numbers.js
+++ Numbers.js
@@ -1,32 +1,3 @@
-// TRIANGLES
-
-let n = 4;
-console.log("This is my first triangle!");
-console.log("__________________________");
-for (let i = 1; i <= n; i++) {
-    let sym = "* ";
-    console.log(sym.repeat(i));
-}
-console.log("__________________________");
-
-let m = 5;
-console.log("This is my second triangle!");
-console.log("__________________________");
-for (let i = 1; i <= m; i++) {
-    let kym = "$ ";
-    console.log(kym.repeat((m - i)));
-}
-console.log("__________________________");
-
-let o = 5;
-console.log("Random figure");
-console.log("__________________________");
-for (let i = 1; i <= o; i++) {
-    let kym = "$ ";
-    console.log(kym.repeat((o + 3 + i)));
-}
-console.log("__________________________");
-
 // КРАТНІСТЬ ЧИСЕЛ
 
 let sum = 0;
