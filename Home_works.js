290455a068b1db90254d99b8d009b51c00d88ff6
Karged <andrii.briezghunov@gmail.com>
2024-03-18 14:24:11 +0200

    little triangles

--- Home_works.js
+++ Home_works.js
@@ -1,27 +1,13 @@
-let age = "gdhg";
-let age1 = age % 10;
-let message = '';
-if (age < 1 || age > 99) {
-    message = " років? та ну не може такого бути!";
-}
-else if (age == 1) {
-    message = " рік";
-}
-else if (age < 5 && age > 1) {
-    message = " роки";
-}
-else if (age > 4 && age < 21) {
-    message = " років";
-}
-else if (age > 20 && age1 == 1) {
-    message = " рік";
-}
-else if (age > 20 && age1 > 1 && age1 < 5) {
-    message = " роки";
-}
-else {
-    message = " років";
-};
-console.log("тобі " + age + message);
-console.log(typeof age);
 
+let n = 4;
+for (let i = 1; i <= n; i++) {
+    let sym = "* ";
+    console.log(sym.repeat(i));
+}
+
+let m = 5;
+for (let i = 1; i <= m; i++) {
+    let kym = "& ";
+    let gym = ' ';
+    console.log(kym.repeat((m-i))+gym.repeat(i));
+}
