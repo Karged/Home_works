9a8f9fabba892f71e1df23b9379a4a4871f5d27d
Karged <andrii.briezghunov@gmail.com>
2024-03-19 22:10:40 +0200

    last homework part

--- Numbers.js
+++ Numbers.js
@@ -1,5 +1,5 @@
 // КРАТНІСТЬ ЧИСЕЛ
-
+/*
 let sum = 0;
 for (var i = 1; i <= 100; i++) {
     if (i % 3 != 0)
@@ -20,3 +20,35 @@ while (n <= 100) {
     continue;
 }
 console.log("Пораховано за допомогою while. Сума = " + sim);
+
+*/
+
+/*
+function pow()
+{
+    let a = 3;
+    let b = 4;
+    let i = 1;
+    let c = a;
+    while (i <= b - 1) {
+        c = c * a;
+        i++;
+    }
+    console.log(c);
+}
+str();
+
+*/
+// Функція a в ступені b
+function pow(a, b) {
+    let i = 1;
+    let c = a;
+    while (i != b) {
+        c = c * a;
+        i++;
+    }
+    return(c);
+}
+    let result = pow(2, 15);
+    console.log(result);
+
