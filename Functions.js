4d52773b8718559fa62234efd71b42c7ef3f71b4
Karged <andrii.briezghunov@gmail.com>
2024-03-23 21:07:20 +0200

    changes

---
+++ Functions.js
@@ -0,0 +1,19 @@
+// My PAD function
+/*
+function mypad(mystr, mysymb, mybool) {
+    let myl = mystr.length;
+    let n = 10;
+    let i = n - myl;
+    let result;
+    if (mybool == true) {
+        result = mysymb.repeat(i) + mystr;
+    }
+    else
+        result = mystr + mysymb.repeat(i);
+
+    return (result);
+}
+let result = mypad('andrii', '$', true);
+console.log(result);
+*/
+let
