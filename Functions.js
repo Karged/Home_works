7a6de3f2fef9c5786b0e17f916ca58abeacd9332
Karged <andrii.briezghunov@gmail.com>
2024-03-26 17:48:10 +0200

    changes

--- Functions.js
+++ Functions.js
@@ -1,4 +1,5 @@
 // My PAD function
+/*
 function mypad(mystr, mysymb, mybool) {
     let myl = mystr.length;
     let n = 10;
@@ -14,4 +15,32 @@ function mypad(mystr, mysymb, mybool) {
 }
 let result = mypad('andrii', '$', true);
 console.log(result);
+*/
+// My Theory function
 
+let parne = 0;
+let neparne = 0;
+let prctprn;
+let prctnprn;
+
+function RnmNumber(min, max, n) {
+    for (let i = 1; i <= n; i++) {
+        rNumber = (min + (Math.floor(Math.random() * (max - min))));
+
+        console.log(rNumber);
+
+        if (rNumber % 2 == 0)
+            parne++;
+        else //neparne
+            neparne++;
+    }
+    prctprn = (parne / (parne + neparne))*100;
+    prctnprn = (neparne / (parne + neparne))*100;
+    console.log("p: " + parne);
+    console.log("np: " + neparne);
+    console.log("Парні: " + prctprn + " %");
+    console.log("Непарні: " + prctnprn + " %");
+
+}
+
+console.log(RnmNumber(100, 1000, 10000));
